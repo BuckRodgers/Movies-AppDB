9fd06693aa9214aa46631af30378af7927d10a46
Ryan <lee.ryanjohn@gmail.com>
2014-06-16 21:10:10 +0200

    Moved project into location
    Finished adding DB
    Removed authentication logic

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
+        }
+
+
+        //
+        // GET: /HelloWorld/
+        /*
+        public ActionResult Index()
+        {
+            return View();
+        }
+         */
+   
+        /*
+        public string Index()    
+        {
+            
+            return "this is my <b>default</b> action...";
+        }
+        */
+        // Get /HelloWorld/Welcome
+        
+        //HelloWorld/Welcome?name=ryan&numTimes=5
+        /*
+        public string Welcome(String name, int numTimes = 1)
+        {
+            //protection against javascript maliciuos input
+            return HttpUtility.HtmlEncode("Hello " + name + ", NumTimes is: " + numTimes);
+            //return "this is the welcome action method....";
+        }
+         * */
+	}
+}
---
+++ MvcMovie/Models/Movie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity; // 2 for the context
+
+namespace MvcMovie.Models
+{
+    public class Movie  //POCO class
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class MovieDBContext : DbContext //represents entity framework movie database context
+    {
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
